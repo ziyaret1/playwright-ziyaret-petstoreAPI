f3ffbb90b9027718377d336c79e87052a086ed86
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-10-24 18:52:22 +0400

    User Management dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 // Imporst for .env
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({ path: path.resolve(process.cwd(), ".env"), quiet: true });
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 export default defineConfig({
   testDir: "./tests",
