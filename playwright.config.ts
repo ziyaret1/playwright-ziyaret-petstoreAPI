4d191e5dad7608583b50ac68941f2c1392120f20
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-10-24 16:01:55 +0400

    User Management dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,8 +9,11 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 3 : undefined,
-  reporter: [["list"], ["allure-playwright"]],
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+  ["list"],
+  ["allure-playwright", { outputFolder: "allure-results" }]
+],
   use: {
     trace: "on-first-retry",
   },
